966d542eb2e0fea42e0cc8ad46cce14a2842eff3
yujitakai <yuji21eagles0912@yahoo.co.jp>
2018-01-29 10:58:24 -0500

    I worked on Person 5 - Yuji

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Yuji Takai
  *  @version 1.1
  */
 public class Person5 {
@@ -31,7 +31,8 @@ public class Person5 {
 	 */
 	private String calc(String input) {
 	  //Person 5 put your implementation here
-	  return null;
+	  return input.substring(input.length() - 2, input.length())
+			  + input.substring(0, input.length() - 2);
 	}
 	
 	/**
