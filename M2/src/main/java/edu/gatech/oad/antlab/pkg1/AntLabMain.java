9fe2f225eeebd5d0012dab54e5bb4f2f5951dba9
alexyang50870 <alexyang50870@gmail.com>
2018-01-29 13:09:50 -0500

    Moved spaces

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -67,7 +67,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("Alex Yang");
 		  toPrint += p3.toString("ayang76");
-                  //Person4 replace P4 with your name
+		            //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("P4");
                   toPrint += p4.toString("gburdell4");
